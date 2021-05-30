0d42acb941b4a867ea7cb1456cedb76e4ded6e41
Rui Costa <ruicosta042@gmail.com>
2021-05-30 20:10:25 +0100

    tests: add E2E tests commonjs/typescript with jest

---
+++ e2e/main-ts.test.ts
@@ -0,0 +1,18 @@
+import { testEnvsubst } from './main-ts';
+
+describe('envsubst', () => {
+  const oldEnv = process.env;
+
+  afterEach(() => {
+    process.env = oldEnv;
+  });
+
+  it('is testable with Jest and JavaScript', () => {
+    expect.assertions(1);
+    process.env = { ENV_VAR1: 'foo' };
+    const result = testEnvsubst('$ENV_VAR1');
+
+    const expected = 'foo';
+    expect(result).toStrictEqual(expected);
+  });
+});
